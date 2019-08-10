3616d63c1b921daf406a6d548eb5f8206afaea37
Alexander Komarov <komar81@gmail.com>
2019-08-10 13:53:38 +0300

    50% done

--- src/components/render.js
+++ src/components/render.js
@@ -1,3 +1,9 @@
 export const renderComponent = (container, component, place) => {
   container.insertAdjacentHTML(place, component);
 };
+
+export const getComponent = (markup) => {
+  const htmlTemplate = document.createElement(`template`);
+  htmlTemplate.innerHTML = markup.trim();
+  return htmlTemplate.content.firstElementChild.cloneNode(true);
+};
